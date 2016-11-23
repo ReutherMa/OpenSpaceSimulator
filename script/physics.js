04765cbc75fb9e01a7be86aae88afa3f025fe029
ReutherMa <marius_reuther@gmx.de>
2016-11-23 18:32:33 +0100

    Repaired Project

--- script/physics.js
+++ script/physics.js
@@ -67,16 +67,6 @@ function calculateGravitation (difftime, spaceObjects, spaceObject) {
             spaceObject.group.position.z += spaceObject.speedz * difftime;
         }
         
-        dist = 1 / dist;
-        spaceObject.speedx += accel * rx * dist * difftime;
-        spaceObject.speedy += accel * ry * dist * difftime;
-        spaceObject.speedz += accel * rz * dist * difftime;
-        
-        //movement in x direction
-        //spaceObject.group.position.x += 1000000;
-        spaceObject.group.position.x += spaceObject.speedx * difftime;
-        spaceObject.group.position.y += spaceObject.speedy * difftime;
-        spaceObject.group.position.z += spaceObject.speedz * difftime;
     }  
 }
 
