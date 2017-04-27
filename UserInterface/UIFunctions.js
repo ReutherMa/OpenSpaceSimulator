ccccf7db7ac0ba83d68d6529cf8bc052535b4858
SelinaForster <selina-forster@web.de>
2017-04-27 15:46:24 +0200

    interface tabs
    rocket- and developer interface tabs

--- UserInterface/UIFunctions.js
+++ UserInterface/UIFunctions.js
@@ -6,5 +6,22 @@ function prompt(errorstring) {
 
 
 function doToggle(button) {
-    $("#" + button.id + "Interface").slideToggle();
+    $(".interfaceTab").hide(100);
+    if ($("#" + button.id + "Interface").is(':visible')) {
+        $("#" + button.id + "Interface").hide(100);
+    } else {
+        $("#" + button.id + "Interface").show(100);
+    }
+    //$("#" + button.id + "Interface").slideToggle();
+    
 }
+
+/*
+$(".question").click(function () {
+                var questionId = this.id;
+                var answerId = "#" + questionId + "tab";
+                $(".answer").hide(100);
+                $(answerId).show(100);
+                
+            });
+*/
